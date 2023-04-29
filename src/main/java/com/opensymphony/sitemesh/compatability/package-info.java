fb09b28b074891b1aed8f3e830ac264bc9de41e7
Jeremy Landis <jeremylandis@hotmail.com>
2023-04-29 12:54:26 -0400

    [migrate] Move files into maven layout
    - delete old csv ignore
    - convert package.html (legacy) to package-info.java
    - split out resources to resources folder per maven standards
    - move src and test into proper packaging

---
+++ src/main/java/com/opensymphony/sitemesh/compatability/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Compatability classes to enable easy migration of SiteMesh 1 and 2 applications to SiteMesh 3.
+ */
+package com.opensymphony.sitemesh.compatability;
---
+++ src/main/java/com/opensymphony/sitemesh/webapp/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Provides integration of SiteMesh with Java web-applications.
+ */
+package com.opensymphony.sitemesh.webapp;
