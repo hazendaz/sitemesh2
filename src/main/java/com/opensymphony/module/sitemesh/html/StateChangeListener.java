7ee84e575b21643f4f13c6df41b508149ebb73c7
Jeremy Landis <jeremylandis@hotmail.com>
2023-04-29 18:20:10 -0400

    [javadoc] Add missed javadoc that is more broadly filled in by jautodoc and needed help

--- src/main/java/com/opensymphony/module/sitemesh/html/StateChangeListener.java
+++ src/main/java/com/opensymphony/module/sitemesh/html/StateChangeListener.java
@@ -13,6 +13,16 @@
  */
 package com.opensymphony.module.sitemesh.html;
 
+/**
+ * The listener interface for receiving stateChange events. The class that is interested in processing a stateChange
+ * event implements this interface, and the object created with that class is registered with a component using the
+ * component's <code>addStateChangeListener</code> method. When the stateChange event occurs, that object's appropriate
+ * method is invoked.
+ */
 public interface StateChangeListener {
+
+    /**
+     * State finished.
+     */
     void stateFinished();
 }
