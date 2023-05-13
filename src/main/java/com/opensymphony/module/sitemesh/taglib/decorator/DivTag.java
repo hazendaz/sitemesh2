b77d100c4811156f678df2b945cdb4898fd664ab
Jeremy Landis <jeremylandis@hotmail.com>
2023-05-13 13:36:07 -0400

    [ci] copyright date

--- src/main/java/com/opensymphony/module/sitemesh/taglib/decorator/DivTag.java
+++ src/main/java/com/opensymphony/module/sitemesh/taglib/decorator/DivTag.java
@@ -1,7 +1,7 @@
 /*
  * sitemesh2 (https://github.com/hazendaz/sitemesh2)
  *
- * Copyright 2011-2023 Hazendaz.
+ * Copyright 2011 Hazendaz.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of The Apache Software License,
