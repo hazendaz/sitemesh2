bcdf975d837ca287e1ea91f7588d56841e95aec9
Jeremy Landis <jeremylandis@hotmail.com>
2024-03-23 21:15:31 -0400

    [upstream] Apply https://github.com/sitemesh/sitemesh2/commit/f7cbab1b18c9c4e40de8b48d56cc73b03e3fda3c

--- src/main/java/com/opensymphony/sitemesh/webapp/decorator/NoDecorator.java
+++ src/main/java/com/opensymphony/sitemesh/webapp/decorator/NoDecorator.java
@@ -56,16 +56,14 @@ public class NoDecorator extends BaseWebAppDecorator {
     protected void render(Content content, HttpServletRequest request, HttpServletResponse response,
             ServletContext servletContext, SiteMeshWebAppContext webAppContext) throws IOException, ServletException {
 
+        PrintWriter writer;
         if (webAppContext.isUsingStream()) {
-            PrintWriter writer = new PrintWriter(response.getOutputStream());
-            content.writeOriginal(writer);
-            writer.flush(); // flush writer to underlying outputStream
-            response.getOutputStream().flush();
+            writer = new PrintWriter(response.getOutputStream());
         } else {
-            PrintWriter writer = response.getWriter();
-            content.writeOriginal(writer);
-            response.getWriter().flush();
+            writer = response.getWriter();
         }
+        content.writeOriginal(writer);
+        writer.flush();
     }
 
 }
